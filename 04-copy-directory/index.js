fba7fcbf87c11ae2b339f43939d2de3dea03f3f2
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-05-04 21:04:35 +0300

    feat: add solution for 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require('fs');
+const path = require('path');
 
+const sourcePath = path.join(__dirname, 'files');
+const targetPath = path.join(__dirname, 'files-copy');
+const optionsMkDir = { recursive: true };
+const optionsReadDir = { withFileTypes: true };
+
+function copyFiles() {
+  fs.promises.readdir(sourcePath, optionsReadDir)
+    .then(filenames => {
+      filenames.forEach(file => {
+        if (file.isFile()) {
+          const sFile = path.join(sourcePath, file.name);
+          const tFile = path.join(targetPath, file.name);
+          fs.promises.copyFile(sFile, tFile)
+            .catch(error => console.log(error))
+        }
+      });
+    })
+    .catch(error => console.log(error))
+    .then(() => console.log('Файлы скопированы'));
+}
+
+fs.promises.mkdir(targetPath, optionsMkDir)
+  .catch((error) => console.log(error))
+  .then(() => copyFiles());
