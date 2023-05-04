cd6c5a1f1928dc20ac331ff4d95f61c03a088e62
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-05-04 15:46:51 +0300

    feat: add solution for 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,15 @@
+const { stdin, stdout } = process;
+const fs = require('fs');
+const path = require('path');
+
+const fullPath = path.join(__dirname, 'text.txt');
+const wStream = fs.createWriteStream(fullPath);
+
+stdin.on('data', data => {
+    data.toString().replace('\r\n', '') === 'exit' ? process.emit('SIGINT') : wStream.write(data);
+});
+process.on('SIGINT', () => {
+    stdout.write('\nВвод данных окончен, спасибо!\n');
+    process.exit();
+});
+wStream.on('error', error => console.log('Error', error.message));
