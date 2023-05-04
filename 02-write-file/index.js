93f4d3175678a32ed5ebb7ad3ae96831277d237e
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-05-04 18:50:51 +0300

    fix: change num of spaces

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -6,10 +6,10 @@ const fullPath = path.join(__dirname, 'text.txt');
 const wStream = fs.createWriteStream(fullPath);
 
 stdin.on('data', data => {
-    data.toString().replace('\r\n', '') === 'exit' ? process.emit('SIGINT') : wStream.write(data);
+  data.toString().replace('\r\n', '') === 'exit' ? process.emit('SIGINT') : wStream.write(data);
 });
 process.on('SIGINT', () => {
-    stdout.write('\nВвод данных окончен, спасибо!\n');
-    process.exit();
+  stdout.write('\nВвод данных окончен, спасибо!\n');
+  process.exit();
 });
 wStream.on('error', error => console.log('Error', error.message));
