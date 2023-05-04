54877bfd0ebc9cfb1f20649a8407da607d6fc827
Philipp Dobrikov <philipp.dobrikov@yandex.ru>
2023-05-04 18:49:32 +0300

    feat: add solution for 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+
+const fullPath = path.join(__dirname, 'secret-folder');
+const options = { withFileTypes: true };
+
+fs.readdir(fullPath, options, (error, files) => {
+  if (error)
+    console.log(error);
+  else {
+    files.forEach(file => {
+      if(file.isFile()) {
+        const name = file.name.split('.').slice(0,-1).join();
+        const extname = path.extname(file.name).substring(1);
+        const filePath = path.join(fullPath, file.name);
+        fs.stat(filePath, (error, stats) => {
+          if (error)
+            console.log(error);
+          else { 
+            size = (stats.size / 1024).toFixed(3);
+            console.log(`${name} - ${extname} - ${size}kb`);
+          }
+        });
+      }
+    })
+  }
+})
